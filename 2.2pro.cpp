e1c6247cc23b4a8bdb633b91830bb5a946c9eda4
saisanju2003 <101688507+saisanju2003@users.noreply.github.com>
2022-10-03 11:04:35 +0530

    Create 2.2pro.cpp

---
+++ 2.2pro.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	float number1, number2, sum, average;
+	cout<<"enter two numbers:";
+	cin >> number1;
+	cin>>number2;
+	sum=number1+number2;
+	average+sum/2;
+	
+	cout<<"sum="<<sum<<"\n";
+	cout<<"average="<<average<<"\n";
+	return 0;
+}
