3fb24dc02a4505bf65cd5f5268f0755a014a375b
saisanju2003 <101688507+saisanju2003@users.noreply.github.com>
2022-10-03 11:05:27 +0530

    Create 3.4pro.cpp

---
+++ 3.4pro.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip> //for setw
+using namespace std;
+int main()
+{
+	int basic = 950, allowance = 95, total = 1045
+	
+	cout << setw(10) <<"basic" <<setw(10) <<basic <<endl
+	<<setw(10)<< "allowance"<<setw(10)<<allowance<<endl
+	<<setw(10)<<"total"<<setw(10)<<total<<endl;
+	return 0;
+}
